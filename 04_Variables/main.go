b2c5a8d33efb06a29a0af15daddb781978a9c05f
Hitesh8411 <hiteshsuryavanshi2004@gmail.com>
2026-06-01 23:56:54 +0530

    Variables

---
+++ 04_Variables/main.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+		
+
+
+func main(){
+	fmt.Println("learning about how to import packages")
+
+	var name string ="Hitesh"
+	fmt.Println(name)
+
+	person := "123"
+	fmt.Println(person) 
+}
